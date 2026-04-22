35c8eb6f8cd7762593eda1c32593fde27ef050bd
amitkumar linge <amitlinge410@gmail.com>
2026-04-22 14:25:32 +0530

    user register post method

---
+++ CargoConnect/CargoConnect.API/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using CargoConnect.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CargoConnect.API.Controllers
+{
+    [ApiController]
+    [Route("Api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        IUserService _userService;
+
+        public AccountController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("Register-User")]
+        public async Task<IActionResult> RegisterUser(UserCreateDTO userCreateDto)
+        {
+            if(ModelState.IsValid)
+            {
+                bool status = await _userService.CreateAsync(userCreateDto);
+                if (status)
+                {
+                    return Created("",userCreateDto);
+                }
+                return BadRequest();
+            }
+            return BadRequest();
+        }
+    }
+}
