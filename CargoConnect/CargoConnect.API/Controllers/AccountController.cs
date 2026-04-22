255fa993ff99804070a9b1cad462cac8340b174f
amitkumar linge <amitlinge410@gmail.com>
2026-04-22 14:40:48 +0530

    Driver register method

--- CargoConnect/CargoConnect.API/Controllers/AccountController.cs
+++ CargoConnect/CargoConnect.API/Controllers/AccountController.cs
@@ -8,10 +8,11 @@ namespace CargoConnect.API.Controllers
     public class AccountController : ControllerBase
     {
         IUserService _userService;
-
-        public AccountController(IUserService userService)
+        IDriverService _driverService;
+        public AccountController(IUserService userService, IDriverService driverService)
         {
             _userService = userService;
+            _driverService = driverService;
         }
 
         [HttpPost("Register-User")]
@@ -23,6 +24,21 @@ namespace CargoConnect.API.Controllers
                 if (status)
                 {
                     return Created("",userCreateDto);
+                }
+                return BadRequest();
+            }
+            return BadRequest();
+        }
+
+        [HttpPost("Register-Driver")]
+        public async Task<IActionResult> RegisterDriver(DriverCreateDTO driverCreateDto)
+        {
+            if (ModelState.IsValid)
+            {
+                bool status = await _driverService.CreateAsync(driverCreateDto);
+                if (status)
+                {
+                    return Created("", driverCreateDto);
                 }
                 return BadRequest();
             }
