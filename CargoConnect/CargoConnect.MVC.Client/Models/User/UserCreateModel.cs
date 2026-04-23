a5bd2af71f5e7b55c8998269d4711eecc09d0de8
mugdhakashid <mugdhakashid@gmail.com>
2026-04-23 20:22:01 +0530

    User Create Model

---
+++ CargoConnect/CargoConnect.MVC.Client/Models/User/UserCreateModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CargoConnect.MVC.Client.Models.User
+{
+    public class UserCreateModel
+    {
+        [Required(ErrorMessage = "First name is required")]
+        [MinLength(2, ErrorMessage = "First name must be at least 2 characters")]
+        [MaxLength(50)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Last name is required")]
+        [MinLength(2, ErrorMessage = "Last name must be at least 2 characters")]
+        [MaxLength(50)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [RegularExpression(@"^[6-9]\d{9}$", ErrorMessage = "Invalid phone number")]
+        public string Phone { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(6)]
+        [MaxLength(20)]
+        [RegularExpression(
+            @"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&]).{6,}$",
+            ErrorMessage = "Password must contain uppercase, lowercase, number and special character"
+        )]
+        public string Password { get; set; } = string.Empty;
+    }
+}
