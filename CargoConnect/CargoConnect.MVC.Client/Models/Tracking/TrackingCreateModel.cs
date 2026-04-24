7244a43037f476aae81267c9491a04d3b01613f2
jaysh <jayshreethorat17@gmail.com>
2026-04-24 23:08:44 +0530

    Created Tracking Model

---
+++ CargoConnect/CargoConnect.MVC.Client/Models/Tracking/TrackingCreateModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CargoConnect.MVC.Client.Models.Tracking
+{
+    public class TrackingCreateModel
+    {
+
+
+        [Required]
+        public Guid BookingId { get; set; }
+
+        [Required]
+        public double Latitude { get; set; }
+
+        [Required]
+        public double Longitude { get; set; }
+
+        [Required]
+        public DateTime Timestamp { get; set; }
+    }
+}
+   
