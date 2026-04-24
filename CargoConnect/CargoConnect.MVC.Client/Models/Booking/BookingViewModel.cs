d34e2abc4f3e57e38912b9ae36107c76528936bb
Ankita Khade <ankita24.net@gmail.com>
2026-04-24 10:37:36 +0530

    Create BookingViewModel

---
+++ CargoConnect/CargoConnect.MVC.Client/Models/Booking/BookingViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CargoConnect.Common.Enums;
+using CargoConnect.Domain.Entities;
+
+namespace CargoConnect.MVC.Client.Models.Booking
+{
+    public class BookingViewModel
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public UserEntity? User { get; set; }
+
+        public Guid DriverId { get; set; }
+        public DriverEntity? Driver { get; set; }
+        public string PickupLocation { get; set; }
+        public string DropLocation { get; set; }
+        public BookingStatus Status { get; set; } = BookingStatus.Pending;
+        public double Distance { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
