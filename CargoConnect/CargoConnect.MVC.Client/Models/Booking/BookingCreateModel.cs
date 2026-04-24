c0e788d11d7f193747e52b654055c7e9988f2e33
Ankita Khade <ankita24.net@gmail.com>
2026-04-24 10:36:10 +0530

    Create BookingModel

---
+++ CargoConnect/CargoConnect.MVC.Client/Models/Booking/BookingCreateModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CargoConnect.MVC.Client.Models.Booking
+{
+    public class BookingCreateModel
+    {
+        [Required(ErrorMessage = "UserId is required")]
+        public Guid UserId { get; set; }
+
+        [Required(ErrorMessage = "DriverId is required")]
+        public Guid DriverId { get; set; }
+
+        [Required(ErrorMessage = "Pickup location is required")]
+        [MinLength(2, ErrorMessage = "Pickup location must be at least 2 characters")]
+        public string PickupLocation { get; set; }
+
+        [MinLength(2, ErrorMessage = "Drop location must be at least 2 characters")]
+        public string DropLocation { get; set; }
+
+        [Range(1, double.MaxValue, ErrorMessage = "Distance must be greater than 0")]
+        public double Distance { get; set; }
+
+        [Range(1, 100000, ErrorMessage = "Price must be between 1 and 100000")]
+        public decimal Price { get; set; }
+    }
+}
